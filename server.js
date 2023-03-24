dc64057b2461d986c2224593e8edb6401ba919bb
bone4collect <100493415+bone4collect@users.noreply.github.com>
2023-03-24 02:25:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import  express  from "express"
+const express = require('express')
 import Gun from "gun"
-import cors from "cors"
 
 const app = express()
 const port = process.env.PORT || 9000
-app.use(cors())
+
 app.use(Gun.serve)
 app.get('/',(_, res)=> {
     res.status(200).send('> DEBUG: Solciety Node is Live')
